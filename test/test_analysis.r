ed3b0ad783a4f2fa779232e3648b0ddaa2f4c975
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2023-07-19 15:18:25 +0200

    add pb args

--- test/test_analysis.r
+++ test/test_analysis.r
@@ -6,21 +6,29 @@ source("analysis1.r")
 data_sets <- read_csv("test/test_data_sets.csv") %>%
     filter(
         true_sex_cond == 1 &
-        participant_id <= 10
+            participant_id <= 10 &
+            data_set < 3
     )
+
+pb_prob_pos <- 0.9 # prob if b above zero and b lower above zero
+pb_prob_null <- 0.2 # prob if b above zero and b lower below zero OR b below zero and b upper above zero
+pb_prob_neg <- 0.6 # prob if b below zero and b upper below zero
 
 analysis_df <- do_analyses(
     data_sets = data_sets,
     do_pp_linear = TRUE,
-    do_pp_citation = TRUE,
-    do_publication_bias = TRUE
-    )
+    do_pp_citation = FALSE,
+    do_publication_bias = FALSE,
+    pb_prob_pos = pb_prob_pos,
+    pb_prob_neg = pb_prob_neg,
+    pb_prob_null = pb_prob_null
+)
 
 analysis_df <- analysis_df %>%
-                    mutate(
-                      repeat_id = 1,
-                      true_base = 0,
-                      true_sex = 0,
-                      true_cond = 0,
-                      true_sex_cond = -2
-                    )
+    mutate(
+        repeat_id = 1,
+        true_base = 0,
+        true_sex = 0,
+        true_cond = 0,
+        true_sex_cond = 1
+    )
