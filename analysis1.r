81387258bf5419a4f4d7304e8e990b651b717c21
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2023-07-19 13:50:48 +0200

    meta analysis citation

--- analysis1.r
+++ analysis1.r
@@ -4,6 +4,10 @@ do_analyses <- function(
     do_pp_citation,
     do_publication_bias) {
   print(">>>>>>>> Doing analyses")
+
+  n_experiments_per_repeat <- max(data_sets$data_set)
+
+  # make empty dataframes
   results_df <- dplyr::tibble(
     expt = numeric(),
     analysis_type = character(),
@@ -45,8 +49,8 @@ do_analyses <- function(
     pp_u <- 0
     pp_sig <- 0.1
 
-    for (experiment in 1:n_experiments_per_repeat) {
-      print(paste(">>>> Model", experiment, "for linear_no_pb, in repeat", rep, "with true effect", b_sex_cond, sep = " "))
+    for (experiment in seq_len(n_experiments_per_repeat)) {
+      print(paste(">>>> Model", experiment, "for linear_no_pb")) # , in repeat", rep, "with true effect", b_sex_cond, sep = " "))
       # for every experiment get the relevant data set
       this_data_set <- data_sets[data_sets$data_set == experiment, ]
 
@@ -128,8 +132,8 @@ do_analyses <- function(
       pp_u <- 0
       pp_sig <- 0.1
 
-      for (experiment in 1:n_experiments_per_repeat) {
-        print(paste(">>>> Model", experiment, "for linear_sym, in repeat", rep, "with true effect", b_sex_cond, sep = " "))
+      for (experiment in seq_len(n_experiments_per_repeat)) {
+        print(paste(">>>> Model", experiment, "for linear_sym")) # , in repeat", rep, "with true effect", b_sex_cond, sep = " "))
         # for every experiment get the relevant data set
         this_data_set <- data_sets[data_sets$data_set == experiment, ]
 
@@ -224,8 +228,8 @@ do_analyses <- function(
       pp_u <- 0
       pp_sig <- 0.1
 
-      for (experiment in 1:n_experiments_per_repeat) {
-        print(paste(">>>> Model", experiment, "for linear_asym, in repeat", rep, "with true effect", b_sex_cond, sep = " "))
+      for (experiment in seq_len(n_experiments_per_repeat)) {
+        print(paste(">>>> Model", experiment, "for linear_asym")) # , in repeat", rep, "with true effect", b_sex_cond, sep = " "))
         # for every experiment get the relevant data set
         this_data_set <- data_sets[data_sets$data_set == experiment, ]
 
@@ -324,18 +328,17 @@ do_analyses <- function(
     ## WITHOUT PUBLCIATION BIAS ##
 
     print(">>>>>>>>>>>> Doing posterior passing on citation chain (no pb)")
-    # prep columns
-    # this function is in util.R
-    save_analysis_results_1("pp_citation")
-    pub_method <<- c(pub_method, rep(NA, n_experiments_per_repeat))
+    # name analysis
+    current_analysis_type <- "pp_citation"
+    current_pub_method <- NA
 
     # empty df to save data in each iteration
     chain_df <- as.data.frame(matrix(0, nrow = 0, ncol = 3))
     colnames(chain_df) <- c("pp_u", "pp_sig", "studyID")
 
     # loop
-    for (experiment in 1:n_experiments_per_repeat) {
-      print(paste(">>>> Model", experiment, "for citation_no_pb, in repeat", rep, "with true effect", b_sex_cond, sep = " "))
+    for (experiment in seq_len(n_experiments_per_repeat)) {
+      print(paste(">>>> Model", experiment, "for citation_no_pb")) # , in repeat", rep, "with true effect", b_sex_cond, sep = " "))
       # for every experiment get the relevant data set
       this_data_set <- data_sets[data_sets$data_set == experiment, ]
       this_data_set$studyID <- as.character(this_data_set$studyID)
@@ -347,7 +350,7 @@ do_analyses <- function(
         pp_u <- 0
         pp_sig <- 0.1
 
-        pp_n <<- c(pp_n, 0)
+        pp_n <- 0
       } else {
         this_citation_chain <- citation_chain %>%
           filter(to == this_data_set$studyID[1]) %>%
@@ -361,7 +364,7 @@ do_analyses <- function(
           pp_u <- 0
           pp_sig <- 0.1
 
-          pp_n <<- c(pp_n, 0)
+          pp_n <- 0
         } else {
           this_chain_df <- chain_df %>% filter(chain_df$studyID %in% this_citation_chain$from)
 
@@ -369,14 +372,14 @@ do_analyses <- function(
             pp_u <- this_chain_df$pp_u
             pp_sig <- this_chain_df$pp_sig
 
-            pp_n <<- c(pp_n, nrow(this_chain_df))
+            pp_n <- nrow(this_chain_df)
           } else {
             pp <- kalman(mean = this_chain_df$pp_u, sd = this_chain_df$pp_sig)
 
             pp_u <- pp[, 1]
             pp_sig <- pp[, 2]
 
-            pp_n <<- c(pp_n, nrow(this_chain_df))
+            pp_n <- nrow(this_chain_df)
           }
         }
       }
@@ -406,42 +409,46 @@ do_analyses <- function(
         cores = 2
       )
 
-      # save the results of the pp
-      b_base_med <<- c(b_base_med, fixef(model)[, 1][[1]])
-      b_sex_med <<- c(b_sex_med, fixef(model)[, 1][[2]])
-      b_cond_med <<- c(b_cond_med, fixef(model)[, 1][[3]])
-      b_sex_cond_med <<- c(b_sex_cond_med, fixef(model)[, 1][[4]])
-
-      b_base_lower <<- c(b_base_lower, fixef(model)[, 3][[1]])
-      b_sex_lower <<- c(b_sex_lower, fixef(model)[, 3][[2]])
-      b_cond_lower <<- c(b_cond_lower, fixef(model)[, 3][[3]])
-      b_sex_cond_lower <<- c(b_sex_cond_lower, fixef(model)[, 3][[4]])
-
-      b_base_upper <<- c(b_base_upper, fixef(model)[, 4][[1]])
-      b_sex_upper <<- c(b_sex_upper, fixef(model)[, 4][[2]])
-      b_cond_upper <<- c(b_cond_upper, fixef(model)[, 4][[3]])
-      b_sex_cond_upper <<- c(b_sex_cond_upper, fixef(model)[, 4][[4]])
-
-      b_base_error <<- c(b_base_error, fixef(model)[, 2][[1]])
-      b_sex_error <<- c(b_sex_error, fixef(model)[, 2][[2]])
-      b_cond_error <<- c(b_cond_error, fixef(model)[, 2][[3]])
-      b_sex_cond_error <<- c(b_sex_cond_error, fixef(model)[, 2][[4]])
-
-      b_sex_p_value <<- c(b_sex_p_value, NaN)
-      b_cond_p_value <<- c(b_cond_p_value, NaN)
-      b_sex_cond_p_value <<- c(b_sex_cond_p_value, NaN)
-
       # new dataframe with saved PP values
-
       this_chain_df$pp_u <- fixef(model)[, 1][[4]]
       this_chain_df$pp_sig <- fixef(model)[, 2][[4]]
       this_chain_df$studyID <- this_data_set$studyID[1]
 
-      # binding into df with all PP values
-
       chain_df <- rbind(chain_df, this_chain_df)
 
-      pub_true <<- c(pub_true, 1)
+      # save the results of the pp
+      tmp_results_df <- dplyr::tibble(
+        expt = experiment,
+        analysis_type = current_analysis_type,
+        pub_method = current_pub_method,
+        pub_true = 1,
+        posteriors_included = pp_n,
+        b_base_med = fixef(model)[, 1][[1]],
+        b_sex_med = fixef(model)[, 1][[2]],
+        b_cond_med = fixef(model)[, 1][[3]],
+        b_sex_cond_med = fixef(model)[, 1][[4]],
+        b_base_lower = fixef(model)[, 3][[1]],
+        b_sex_lower = fixef(model)[, 3][[2]],
+        b_cond_lower = fixef(model)[, 3][[3]],
+        b_sex_cond_lower = fixef(model)[, 3][[4]],
+        b_base_upper = fixef(model)[, 4][[1]],
+        b_sex_upper = fixef(model)[, 4][[2]],
+        b_cond_upper = fixef(model)[, 4][[3]],
+        b_sex_cond_upper = fixef(model)[, 4][[4]],
+        b_base_error = fixef(model)[, 2][[1]],
+        b_sex_error = fixef(model)[, 2][[2]],
+        b_cond_error = fixef(model)[, 2][[3]],
+        b_sex_cond_error = fixef(model)[, 2][[4]]
+      )
+
+      results_df <- rbind(results_df, tmp_results_df)
+
+      # running meta analysis
+      meta_data <- results_df %>%
+        filter(analysis_type == current_analysis_type & pub_true == 1)
+      tmp_meta_df <- running_meta_analysis(meta_data)
+
+      meta_df <- rbind(meta_df, tmp_meta_df)
     } # end of for each experiment loop
     # end of pp_citation without pb
 
@@ -451,16 +458,16 @@ do_analyses <- function(
       print(">>>>>>>>>>>> Doing posterior passing on citation chain (pb sym)")
       # prep columns
       # this function is in util.R
-      save_analysis_results_1("pp_citation_pb_sym")
-      pub_method <<- c(pub_method, rep("sym", n_experiments_per_repeat))
+      current_analysis_type <- "pp_citation_pb_sym"
+      current_pub_method <- "sym"
 
       # empty df to save data in each iteration
       chain_df <- as.data.frame(matrix(0, nrow = 0, ncol = 3))
       colnames(chain_df) <- c("pp_u", "pp_sig", "studyID")
 
       # loop
-      for (experiment in 1:n_experiments_per_repeat) {
-        print(paste(">>>> Model", experiment, "for citation_sym, in repeat", rep, "with true effect", b_sex_cond, sep = " "))
+      for (experiment in seq_len(n_experiments_per_repeat)) {
+        print(paste(">>>> Model", experiment, "for citation_sym")) # , in repeat", rep, "with true effect", b_sex_cond, sep = " "))
         # for every experiment get the relevant data set
         this_data_set <- data_sets[data_sets$data_set == experiment, ]
         this_data_set$studyID <- as.character(this_data_set$studyID)
@@ -472,7 +479,7 @@ do_analyses <- function(
           pp_u <- 0
           pp_sig <- 0.1
 
-          pp_n <<- c(pp_n, 0) # adding a meassure for number of posteriors included
+          pp_n <- 0 # adding a meassure for number of posteriors included
         } else {
           this_citation_chain <- citation_chain %>%
             filter(to == this_data_set$studyID[1]) %>%
@@ -486,7 +493,7 @@ do_analyses <- function(
             pp_u <- 0
             pp_sig <- 0.1
 
-            pp_n <<- c(pp_n, 0)
+            pp_n <- 0
           } else {
             this_chain_df <- chain_df %>% filter(chain_df$studyID %in% this_citation_chain$from)
 
@@ -494,14 +501,14 @@ do_analyses <- function(
               pp_u <- this_chain_df$pp_u
               pp_sig <- this_chain_df$pp_sig
 
-              pp_n <<- c(pp_n, nrow(this_chain_df))
+              pp_n <- nrow(this_chain_df)
             } else {
               pp <- kalman(mean = this_chain_df$pp_u, sd = this_chain_df$pp_sig)
 
               pp_u <- pp[, 1]
               pp_sig <- pp[, 2]
 
-              pp_n <<- c(pp_n, nrow(this_chain_df))
+              pp_n <- nrow(this_chain_df)
             }
           }
         }
@@ -530,31 +537,6 @@ do_analyses <- function(
           chains = 2,
           cores = 2
         )
-
-        # save the results of the pp
-        b_base_med <<- c(b_base_med, fixef(model)[, 1][[1]])
-        b_sex_med <<- c(b_sex_med, fixef(model)[, 1][[2]])
-        b_cond_med <<- c(b_cond_med, fixef(model)[, 1][[3]])
-        b_sex_cond_med <<- c(b_sex_cond_med, fixef(model)[, 1][[4]])
-
-        b_base_lower <<- c(b_base_lower, fixef(model)[, 3][[1]])
-        b_sex_lower <<- c(b_sex_lower, fixef(model)[, 3][[2]])
-        b_cond_lower <<- c(b_cond_lower, fixef(model)[, 3][[3]])
-        b_sex_cond_lower <<- c(b_sex_cond_lower, fixef(model)[, 3][[4]])
-
-        b_base_upper <<- c(b_base_upper, fixef(model)[, 4][[1]])
-        b_sex_upper <<- c(b_sex_upper, fixef(model)[, 4][[2]])
-        b_cond_upper <<- c(b_cond_upper, fixef(model)[, 4][[3]])
-        b_sex_cond_upper <<- c(b_sex_cond_upper, fixef(model)[, 4][[4]])
-
-        b_base_error <<- c(b_base_error, fixef(model)[, 2][[1]])
-        b_sex_error <<- c(b_sex_error, fixef(model)[, 2][[2]])
-        b_cond_error <<- c(b_cond_error, fixef(model)[, 2][[3]])
-        b_sex_cond_error <<- c(b_sex_cond_error, fixef(model)[, 2][[4]])
-
-        b_sex_p_value <<- c(b_sex_p_value, NaN)
-        b_cond_p_value <<- c(b_cond_p_value, NaN)
-        b_sex_cond_p_value <<- c(b_sex_cond_p_value, NaN)
 
         # publication
         if (fixef(model)[, 1][[4]] > 0 & fixef(model)[, 3][[4]] > 0) {
@@ -577,22 +559,55 @@ do_analyses <- function(
           # binding into df with all PP values
           chain_df <- rbind(chain_df, this_chain_df)
         }
-        pub_true <<- c(pub_true, ifelse(pb == 0, 0, 1))
+
+        # save the results of the pp
+        tmp_results_df <- dplyr::tibble(
+          expt = experiment,
+          analysis_type = current_analysis_type,
+          pub_method = current_pub_method,
+          pub_true = ifelse(pb == 0, 0, 1),
+          posteriors_included = pp_n,
+          b_base_med = fixef(model)[, 1][[1]],
+          b_sex_med = fixef(model)[, 1][[2]],
+          b_cond_med = fixef(model)[, 1][[3]],
+          b_sex_cond_med = fixef(model)[, 1][[4]],
+          b_base_lower = fixef(model)[, 3][[1]],
+          b_sex_lower = fixef(model)[, 3][[2]],
+          b_cond_lower = fixef(model)[, 3][[3]],
+          b_sex_cond_lower = fixef(model)[, 3][[4]],
+          b_base_upper = fixef(model)[, 4][[1]],
+          b_sex_upper = fixef(model)[, 4][[2]],
+          b_cond_upper = fixef(model)[, 4][[3]],
+          b_sex_cond_upper = fixef(model)[, 4][[4]],
+          b_base_error = fixef(model)[, 2][[1]],
+          b_sex_error = fixef(model)[, 2][[2]],
+          b_cond_error = fixef(model)[, 2][[3]],
+          b_sex_cond_error = fixef(model)[, 2][[4]]
+        )
+
+        results_df <- rbind(results_df, tmp_results_df)
+
+        # running meta analysis
+        meta_data <- results_df %>%
+          filter(analysis_type == current_analysis_type & pub_true == 1)
+        tmp_meta_df <- running_meta_analysis(meta_data)
+
+        meta_df <- rbind(meta_df, tmp_meta_df)
       } # end of pp_citation_sym
 
       print(">>>>>>>>>>>> Doing posterior passing on citation chain (pb asym)")
       # prep columns
       # this function is in util.R
-      save_analysis_results_1("pp_citation_pb_asym")
-      pub_method <<- c(pub_method, rep("asym", n_experiments_per_repeat))
+      current_analysis_type <- "pp_citation_pb_asym"
+      current_pub_method <- "asym"
 
       # empty df to save data in each iteration
       chain_df <- as.data.frame(matrix(0, nrow = 0, ncol = 3))
       colnames(chain_df) <- c("pp_u", "pp_sig", "studyID")
 
       # loop
-      for (experiment in 1:n_experiments_per_repeat) {
-        print(paste(">>>> Model", experiment, "for citation_asym, in repeat", rep, "with true effect", b_sex_cond, sep = " "))
+      for (experiment in seq_len(n_experiments_per_repeat)) {
+        print(paste(">>>> Model", experiment, "for citation_asym")) # , in repeat", rep, "with true effect", b_sex_cond, sep = " "))
         # for every experiment get the relevant data set
         this_data_set <- data_sets[data_sets$data_set == experiment, ]
         this_data_set$studyID <- as.character(this_data_set$studyID)
@@ -604,7 +619,7 @@ do_analyses <- function(
           pp_u <- 0
           pp_sig <- 0.1
 
-          pp_n <<- c(pp_n, 0) # adding a meassure for number of posteriors included
+          pp_n <- 0 # adding a meassure for number of posteriors included
         } else {
           this_citation_chain <- citation_chain %>%
             filter(to == this_data_set$studyID[1]) %>%
@@ -618,7 +633,7 @@ do_analyses <- function(
             pp_u <- 0
             pp_sig <- 0.1
 
-            pp_n <<- c(pp_n, 0)
+            pp_n <- 0
           } else {
             this_chain_df <- chain_df %>% filter(chain_df$studyID %in% this_citation_chain$from)
 
@@ -626,14 +641,14 @@ do_analyses <- function(
               pp_u <- this_chain_df$pp_u
               pp_sig <- this_chain_df$pp_sig
 
-              pp_n <<- c(pp_n, nrow(this_chain_df))
+              pp_n <- nrow(this_chain_df)
             } else {
               pp <- kalman(mean = this_chain_df$pp_u, sd = this_chain_df$pp_sig)
 
               pp_u <- pp[, 1]
               pp_sig <- pp[, 2]
 
-              pp_n <<- c(pp_n, nrow(this_chain_df))
+              pp_n <- nrow(this_chain_df)
             }
           }
         }
@@ -662,31 +677,6 @@ do_analyses <- function(
           chains = 2,
           cores = 2
         )
-
-        # save the results of the pp
-        b_base_med <<- c(b_base_med, fixef(model)[, 1][[1]])
-        b_sex_med <<- c(b_sex_med, fixef(model)[, 1][[2]])
-        b_cond_med <<- c(b_cond_med, fixef(model)[, 1][[3]])
-        b_sex_cond_med <<- c(b_sex_cond_med, fixef(model)[, 1][[4]])
-
-        b_base_lower <<- c(b_base_lower, fixef(model)[, 3][[1]])
-        b_sex_lower <<- c(b_sex_lower, fixef(model)[, 3][[2]])
-        b_cond_lower <<- c(b_cond_lower, fixef(model)[, 3][[3]])
-        b_sex_cond_lower <<- c(b_sex_cond_lower, fixef(model)[, 3][[4]])
-
-        b_base_upper <<- c(b_base_upper, fixef(model)[, 4][[1]])
-        b_sex_upper <<- c(b_sex_upper, fixef(model)[, 4][[2]])
-        b_cond_upper <<- c(b_cond_upper, fixef(model)[, 4][[3]])
-        b_sex_cond_upper <<- c(b_sex_cond_upper, fixef(model)[, 4][[4]])
-
-        b_base_error <<- c(b_base_error, fixef(model)[, 2][[1]])
-        b_sex_error <<- c(b_sex_error, fixef(model)[, 2][[2]])
-        b_cond_error <<- c(b_cond_error, fixef(model)[, 2][[3]])
-        b_sex_cond_error <<- c(b_sex_cond_error, fixef(model)[, 2][[4]])
-
-        b_sex_p_value <<- c(b_sex_p_value, NaN)
-        b_cond_p_value <<- c(b_cond_p_value, NaN)
-        b_sex_cond_p_value <<- c(b_sex_cond_p_value, NaN)
 
         # publication
         if (fixef(model)[, 1][[4]] > 0 & fixef(model)[, 3][[4]] > 0) {
@@ -709,7 +699,40 @@ do_analyses <- function(
           # binding into df with all PP values
           chain_df <- rbind(chain_df, this_chain_df)
         }
-        pub_true <<- c(pub_true, ifelse(pb == 0, 0, 1))
+
+        # save the results of the pp
+        tmp_results_df <- dplyr::tibble(
+          expt = experiment,
+          analysis_type = current_analysis_type,
+          pub_method = current_pub_method,
+          pub_true = ifelse(pb == 0, 0, 1),
+          posteriors_included = pp_n,
+          b_base_med = fixef(model)[, 1][[1]],
+          b_sex_med = fixef(model)[, 1][[2]],
+          b_cond_med = fixef(model)[, 1][[3]],
+          b_sex_cond_med = fixef(model)[, 1][[4]],
+          b_base_lower = fixef(model)[, 3][[1]],
+          b_sex_lower = fixef(model)[, 3][[2]],
+          b_cond_lower = fixef(model)[, 3][[3]],
+          b_sex_cond_lower = fixef(model)[, 3][[4]],
+          b_base_upper = fixef(model)[, 4][[1]],
+          b_sex_upper = fixef(model)[, 4][[2]],
+          b_cond_upper = fixef(model)[, 4][[3]],
+          b_sex_cond_upper = fixef(model)[, 4][[4]],
+          b_base_error = fixef(model)[, 2][[1]],
+          b_sex_error = fixef(model)[, 2][[2]],
+          b_cond_error = fixef(model)[, 2][[3]],
+          b_sex_cond_error = fixef(model)[, 2][[4]]
+        )
+
+        results_df <- rbind(results_df, tmp_results_df)
+
+        # running meta analysis
+        meta_data <- results_df %>%
+          filter(analysis_type == current_analysis_type & pub_true == 1)
+        tmp_meta_df <- running_meta_analysis(meta_data)
+
+        meta_df <- rbind(meta_df, tmp_meta_df)
       } # end of pp_citation_asym
     } # end of pp_citation with pb
   } # end of do_pp_citation
@@ -721,7 +744,7 @@ do_analyses <- function(
   results_df <- cbind(results_df, meta_df)
 
   return(results_df)
-} # end of do_analyses_1
+} # end of do_analyses
 
 kalman <- function(mean, sd) {
   for (i in 1:length(mean)) {
